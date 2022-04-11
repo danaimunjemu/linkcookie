98814aefe4d8d705d0d6f142d7fa5120080987c5
incdesigns <93196532+incdesigns@users.noreply.github.com>
2022-04-11 18:30:29 +0200

    App v3.2.0

--- src/app/landing/for-recruiters/for-recruiters.component.ts
+++ src/app/landing/for-recruiters/for-recruiters.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForRecruitersComponent implements OnInit {
 
+  listOfOption: Array<{ label: string; value: string }> = [];
+  listOfTagOptions = []; 
+
   constructor() { }
 
   ngOnInit(): void {
+    const children: Array<{ label: string; value: string }> = [];
+    for (let i = 10; i < 36; i++) {
+      children.push({ label: i.toString(36) + i, value: i.toString(36) + i });
+    }
+    this.listOfOption = children;
   }
 
 }
