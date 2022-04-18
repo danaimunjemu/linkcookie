428b0087b2874c3874f993e0492849894d78a9d6
incdesigns <93196532+incdesigns@users.noreply.github.com>
2022-04-18 12:46:08 +0200

    Environment VAriables111

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  server_url:"http://localhost:3000/api/"
+  // server_url:"http://localhost:3000/api/"
+  server_url:"https://linkcookie-backend.herokuapp.com/api/"
 };
